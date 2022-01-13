0ee931f3b9305bcbb7d60ceb184faf3c498b95f9
heipartho05 <heipartho72@gmail.com>
2022-01-13 20:09:36 +0600

    update reverseArray

--- src/com/partho/ReverseArray.java
+++ src/com/partho/ReverseArray.java
@@ -1,11 +1,24 @@
 package com.partho;
 
+import java.util.Arrays;
+
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr = {19, 18, 17, 16, 15, 14, 13, 12, 11};
+        int[] arr = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void reverse(int arr2[], int start, int end) {
+    static void reverse(int[] arr2) {
+
+        int start = 0;
+        int end = arr2.length - 1;
+
+        while (start < end) {
+            swap(arr2, start, end);
+            start++;
+            end--;
+        }
     }
 
     static void swap(int[] arr3, int index1, int index2) {
@@ -14,44 +27,3 @@ public class ReverseArray {
         arr3[index2] = temp;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-     public static void main(String[] args) {
-
-        int[] arr = {17, 15, 23, 19, 18, 56};
-        reverse(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    static void reverse(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-
-        while (start < end) {
-            // swap
-            swap(arr, start, end);
-            start++;
-            end--;
-        }
-    }
-
-    static void swap(int[] arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
-    }
-*/
